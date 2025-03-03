e9fdceb1565614e238e7e7ec83df862224895fd2
thomas peyrusaubes <thomas.peyrusaubes@ynov.com>
2025-03-03 17:18:13 +0100

    exercice 3 (Arrays)

---
+++ src/fr/ynov/java/easy/Arrays.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.easy;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] tableau = {10, 20, 30, 40, 50};
+
+        System.out.println("affiche les nombres du tableau : ");
+        for(int j=0;j<tableau.length;j++){
+            System.out.print(tableau[j]);
+        }
+    }
+}
