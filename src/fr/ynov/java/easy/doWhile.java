a580c164fb589f90f7f0c62d8e54e934ca16c1c7
thomas peyrusaubes <thomas.peyrusaubes@ynov.com>
2025-03-03 17:08:27 +0100

    exercice 2 (doWhile)

---
+++ src/fr/ynov/java/easy/doWhile.java
@@ -0,0 +1,13 @@
+package fr.ynov.java.easy;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 11);
+    }
+}
+
