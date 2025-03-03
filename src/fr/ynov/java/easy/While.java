4387963b2d49fdcc6c8b71cdd8fd0774fe02c765
thomas peyrusaubes <thomas.peyrusaubes@ynov.com>
2025-03-03 17:04:20 +0100

    exercice 2 (While)

---
+++ src/fr/ynov/java/easy/While.java
@@ -0,0 +1,11 @@
+package fr.ynov.java.easy;
+
+public class While {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+}
