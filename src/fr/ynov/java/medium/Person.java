2798579692a4f71e7c7e0e851095a9f2327f97ea
thomas peyrusaubes <thomas.peyrusaubes@ynov.com>
2025-03-13 16:19:47 +0100

    date de naissance

--- src/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/medium/Person.java
@@ -1,8 +1,10 @@
 package fr.ynov.java.medium;
 
+import java.time.LocalDate;
+
 public class Person {
     String nom;
-    int age;
+    String birthDate;
     String gender;
     float height;
     float weight;
@@ -16,15 +18,16 @@ public class Person {
         allemand,
     }
 
-    public Person(String nom, int age, String gender, float height, float weight, Nationality nationality) {
+    public Person(String nom, String birthDate, String gender, float height, float weight, Nationality nationality) {
         this.nom = nom;
-        this.age = age;
+        this.birthDate = birthDate;
         this.gender = gender;
         this.height = height;
         this.weight = weight;
         this.nationality = nationality;
     }
-    public static void showinfo (){
+
+    public static void showinfo() {
         System.out.println("Nom :");
         System.out.println("Age :");
         System.out.println("Gender :");
@@ -33,6 +36,17 @@ public class Person {
         System.out.println("Nationality :");
 
     }
+
+    public int calculerAge() {
+        // Récupérer la date du jour
+        // Convertir le String birthDate en date
+        // Calculer la différence entre les deux
+        // YYYY-MM-DD
+        LocalDate today = LocalDate.now();
+        LocalDate birthDate = LocalDate.parse(this.birthDate);
+        return today.getYear() - birthDate.getYear();
+    }
+
 
 }
 
